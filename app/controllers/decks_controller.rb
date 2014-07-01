d7416370fa75a72ca6a63d6d6e866d352d5f3a82
Kevin Nguyen <vinh.15@dartmouth.edu>
2014-07-01 23:26:16 +0200

    added actions to controllers and modified routes

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,3 +1,18 @@
 class DecksController < ApplicationController
 
+	def index
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	def create
+	end
+
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,18 @@
 class NotesController < ApplicationRecord
 
+	def index
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	def create
+	end
+
 end
