409b71377bf5f407be7b1d8f0f973684f351ef2e
Kevin Nguyen <kevin.h.nguyen.15@dartmouth.edu>
2014-08-06 17:07:37 +0200

    edit procfile and hirefire initializer

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,17 @@
 HireFire::Resource.configure do |config|
-  config.dyno(:worker) do
+  config.dyno(:all_worker) do
     HireFire::Macro::Sidekiq.queue
+  end
+
+  config.dyno(:mailer) do
+    HireFire::Macro::Sidekiq.queue(:email)
+  end
+
+  config.dyno(:encoder) do
+    HireFire::Macro::Sidekiq.queue(:encode)
+  end
+
+  config.dyno(:multi_queue) do
+    HireFire::Macro::Sidekiq.queue(:foo, :bar, :baz)
   end
 end
